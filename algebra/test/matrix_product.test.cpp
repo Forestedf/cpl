5d0513a0f4c0eeb5c82052cc60725f9d2741e0ff
Forested <ryotaroh2006@gmail.com>
2022-08-25 19:43:08 +0900

    add `algebra/test`

---
+++ algebra/test/matrix_product.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/matrix_product"
+
+#define FAST_IO
+
+#include "../../template/template.hpp"
+#include "../../number_theory/mod_int.hpp"
+#include "../matrix.hpp"
+
+using Mint = ModInt<mod998244353>;
+
+int main() {
+    i32 n, m, k;
+    cin >> n >> m >> k;
+    Matrix<Mint> a(n, m), b(m, k);
+    REP(i, n) REP(j, m) {
+        cin >> a(i, j);
+    }
+    REP(i, m) REP(j, k) {
+        cin >> b(i, j);
+    }
+    Matrix<Mint> c = a * b;
+    REP(i, n) REP(j, k) {
+        cout << c(i, j) << " \n"[j + 1 == k];
+    }
+}
